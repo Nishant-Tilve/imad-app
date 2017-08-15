8a1e39f4b877ec3c51ad6613ad2650fbba158271
Nishant-Tilve <nishanttilve@gmail.com>
2017-08-15 18:39:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 
 //move img on click
-var image=document.getElementById('darg');
+var image=document.getElementById('drag');
 image.onClick = function(){
     image.style.marginLeft = '100px';
-    //image.style.marginRight = '100px';
-    //image.style.marginTop = 100;
-    //image.style.marginBottom = 100;
+    image.style.marginRight = '100px';
+    image.style.marginTop = 100;
+    image.style.marginBottom = 100;
     
 };
