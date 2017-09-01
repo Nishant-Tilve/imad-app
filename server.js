e8ee4566a49c2cd837cd1c70ee1c7138a0992382
Nishant-Tilve <nishanttilve@gmail.com>
2017-09-01 23:39:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var config = {
     database: 'nishanttilve',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
 };
 
 var app = express();
